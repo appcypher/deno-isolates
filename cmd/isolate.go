7bbf51b918d4ff1d719b151017876e8a5abbb31f
appcypher <appcypher@outlook.com>
2021-05-01 01:47:06 +0100

    Add isolate example

--- cmd/isolate.go
+++ cmd/isolate.go
@@ -7,11 +7,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello")
+	iso, _ := v8go.NewIsolate() // creates a new JavaScript VM
+	ctx1, _ := v8go.NewContext(iso) // new context within the VM
 
-	ctx, _ := v8go.NewContext()                             // creates a new V8 context with a new Isolate aka VM
-	ctx.RunScript("const add = (a, b) => a + b", "math.js") // executes a script on the global context
-	ctx.RunScript("const result = add(3, 4)", "main.js")    // any functions previously added to the context can be called
-	val, _ := ctx.RunScript("result", "value.js")           // return a value in JavaScript back to Go
-	fmt.Printf("addition result: %s", val)
+	ctx1.RunScript("const multiply = (a, b) => a * b", "math.js")
+	ctx1.RunScript("const result = multiply(3, 4)", "main.js") // any functions previously added to the context can be called
+
+	val, _ := ctx1.RunScript("result", "value.js")        // return a value in JavaScript back to Go
+	fmt.Printf("multiply result: %s\n", val)
+
+	ctx2, _ := v8go.NewContext(iso) // another context on the same VM
+	if _, err := ctx2.RunScript("multiply(3, 4)", "main.js"); err != nil {
+	  // this will error as multiply is not defined in this context
+	  fmt.Println("Error in context:", err)
+	}
 }
